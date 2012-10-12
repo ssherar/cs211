c556862b5a7b97094ae75aa4cfb90cbc8dfc42ac
Sam Sherar <s.sherar@live.com>
2012-10-12 19:19:45 +0100

    Moving the CLWriter
    From Vlew to Controller

---
+++ wordladders/src/uk/ac/aber/dcs/cs221/wordladders/view/Screen.java
@@ -0,0 +1,28 @@
+package uk.ac.aber.dcs.cs221.wordladders.view;
+
+import uk.ac.aber.dcs.cs221.wordladders.controller.*;
+
+public class Screen {
+	private CLWriter cli;
+	
+	public Screen() {
+		cli = new CLWriter();
+		cli.write("----------------------------------------");
+		cli.write(" Welcome to WordLadders assignment");
+		cli.write("             (CS211)");
+		cli.write(" Written by Samuel B Sherar (sbs1)");
+		cli.write("----------------------------------------");
+		cli.write();
+	}
+	
+	public String writeMenu() {
+		cli.write("Menu");
+		cli.write("======");
+		cli.write("1) Generate a Word Ladder from a word");
+		cli.write("2) Discover the smallest change from one word to another");
+		cli.write("0) To quit the application");
+		cli.write();
+		cli.prompt("Please enter a value > ");
+		return "";
+	}
+}
